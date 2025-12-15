f9c38969d0f65e0b8f3b88d119c54dd9839694f5
Luke Munderich <luke.munderich@web.de>
2025-12-15 16:30:19 +0100

    Aufgabe 2 bearbeitet

--- 0002_VARIABLES/Math_Computations/Exercise/main.c
+++ 0002_VARIABLES/Math_Computations/Exercise/main.c
@@ -2,6 +2,17 @@
 
 int main()
 {
+    // variables
+    int x = -2;
+    int y = -3;
+    int z1 = x*x + y*y - x*y +2;
+    int z2 = (x-y)*(x-y)*(x-y) -3;
+    float z3 = (2*x*x*x - 0.5*x*x - x +4)/y;
+
+    // output
+    printf("z1 = %d\n", z1);
+    printf("z2 = %d\n", z2);
+    printf("z3 = %f\n", z3);
 
     return 0;
 }
--- 0002_VARIABLES/Time_Converter/Exercise/main.c
+++ 0002_VARIABLES/Time_Converter/Exercise/main.c
@@ -2,7 +2,25 @@
 
 int main()
 {
+    // variable declaration
+    float milliseconds = 0;
+    float seconds;
+    float minutes;
+    float hours;
+    float days;
 
+    // calculations
+    printf("Please enter a millisecond value: ");
+    scanf("%f", &milliseconds);
+    seconds = milliseconds / 1000;
+    minutes = seconds / 60;
+    hours = minutes / 60;
+    days = hours / 24;
 
+    // outputs
+    printf("seconds: %f\n", seconds);
+    printf("minutes: %f\n", minutes);
+    printf("hours: %f\n", hours);
+    printf("days: %f\n", days);
     return 0;
 }
