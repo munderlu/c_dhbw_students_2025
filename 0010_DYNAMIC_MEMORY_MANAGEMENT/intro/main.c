57904f1bc280d820388e3d7ec8ce519e93dabec4
Luke Munderich <luke.munderich@web.de>
2026-01-12 14:53:06 +0100

    Heap-Speicher eingeführt

--- 0010_DYNAMIC_MEMORY_MANAGEMENT/intro/main.c
+++ 0010_DYNAMIC_MEMORY_MANAGEMENT/intro/main.c
@@ -3,6 +3,26 @@
 
 int main()
 {
+    size_t length = 3;
+    size_t memoryReq = length * sizeof(int);
+
+    printf("address of length: %p\n", (void*)&length);
+    printf("sizeof(int): %lu\n", sizeof(int));
+    printf("memoryReq: %ld\n", memoryReq);
+
+    int *heapArray = (int*)malloc(memoryReq);
+
+    for (size_t i = 0; i < length; i++){
+        heapArray[i] = (int)i * 100;
+    }
+
+    for (size_t i = 0; i < length; i++){
+        printf("heapArray[%ld] = %d\n", i, heapArray[i]);
+        printf("address of heapArray[%ld] = %p\n", i, (void*)&heapArray[i]);
+    }
+
+    free(heapArray);
+    heapArray = NULL; //sonst haben wir einen dangling pointer
 
     return 0;
 }
