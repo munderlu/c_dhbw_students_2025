043c6e4e885196d7181d833d959326e38582d425
Luke Munderich <luke.munderich@web.de>
2026-06-27 10:59:16 +0200

    clang-file hinzugefügt

--- Übung-Klausur-2/heapSort.c
+++ Übung-Klausur-2/heapSort.c
@@ -1,30 +1,30 @@
 #include <stdio.h>
 
 void swap(int *array, int i, int j) {
-  int tmp = array[i];
-  array[i] = array[j];
-  array[j] = tmp;
+    int tmp = array[i];
+    array[i] = array[j];
+    array[j] = tmp;
 }
 
 void printArray(int *array, int n) {
-  for (int i = 0; i < n; i++) {
-    printf("%d ", array[i]);
-  }
-  printf("\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", array[i]);
+    }
+    printf("\n");
 }
 
 void heapify(int *array, int n, int i) {
     int largest = i;
-    int left = 2*i +1;
-    int right = 2*i+2;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
 
-    if(left < n && array[left] > array[largest])
+    if (left < n && array[left] > array[largest])
         largest = left;
 
-    if(right < n && array[right] > array[largest])
+    if (right < n && array[right] > array[largest])
         largest = right;
 
-    if (largest !=i){
+    if (largest != i) {
         swap(array, i, largest);
 
         heapify(array, n, largest);
@@ -32,11 +32,11 @@ void heapify(int *array, int n, int i) {
 }
 
 void heapSort(int *array, int n) {
-    for (int i = n/2 -1; i >= 0; i--){
+    for (int i = n / 2 - 1; i >= 0; i--) {
         heapify(array, n, i);
     }
 
-    for (int i = n-1; i > 0;i--) {
+    for (int i = n - 1; i > 0; i--) {
         swap(array, 0, i);
 
         heapify(array, i, 0);
@@ -45,6 +45,6 @@ void heapSort(int *array, int n) {
 }
 
 int main() {
-  int array[] = {7, 6, 5, 4, 3, 2};
-  heapSort(array, 6);
+    int array[] = {7, 6, 5, 4, 3, 2};
+    heapSort(array, 6);
 }
